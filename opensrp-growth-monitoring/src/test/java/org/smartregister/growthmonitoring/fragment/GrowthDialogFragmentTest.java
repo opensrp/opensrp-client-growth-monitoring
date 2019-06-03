c9ec22b1e9836dc8df1e4e7e41557d3b2f0ad95d
Martin Ndegwa <ndegwamartin@gmail.com>
2019-06-03 11:40:24 +0300

    Fix failing build remove deperected junit reference

--- opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/GrowthDialogFragmentTest.java
+++ opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/GrowthDialogFragmentTest.java
@@ -1,6 +1,7 @@
 package org.smartregister.growthmonitoring.fragment;
 
-import junit.framework.Assert;
+
+import org.junit.Assert;
 
 import org.junit.Test;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
