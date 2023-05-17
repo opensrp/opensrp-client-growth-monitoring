8882f0dbdb9940dffb1ab45ef7ec1105f20862f4
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2023-05-17 15:20:07 +0500

    Increase code coverage

--- opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/HeightMonitoringFragmentTest.java
+++ opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/HeightMonitoringFragmentTest.java
@@ -4,6 +4,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 import android.view.View;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,9 +12,15 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.opensrp.api.constants.Gender;
 import org.robolectric.Robolectric;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.growthmonitoring.BaseUnitTest;
+import org.smartregister.growthmonitoring.GrowthMonitoringLibrary;
 import org.smartregister.growthmonitoring.R;
+import org.smartregister.growthmonitoring.domain.HeightZScore;
+import org.smartregister.growthmonitoring.repository.HeightZScoreRepository;
 import org.smartregister.view.customcontrols.CustomFontTextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ndegwamartin on 2020-04-15.
@@ -34,7 +41,16 @@ public class HeightMonitoringFragmentTest extends BaseUnitTest {
 
     @Test
     public void testThatHeightForAgeViewIsCreated() throws InterruptedException {
+        ArrayList<HeightZScore> heightZScores = new ArrayList<>();
+        heightZScores.add(getHeightZScore(56));
+        heightZScores.add(getHeightZScore(44));
+//
+        HeightZScoreRepository heightZScoreRepository = Mockito.spy(GrowthMonitoringLibrary.getInstance().heightZScoreRepository());
+        Mockito.doReturn(heightZScores).when(heightZScoreRepository).findByGender(Mockito.any());
+        ReflectionHelpers.setField(GrowthMonitoringLibrary.getInstance(), "heightZScoreRepository", heightZScoreRepository);
+
         HeightMonitoringFragment fragment = Mockito.spy(HeightMonitoringFragment.createInstance("2018-09-12", Gender.FEMALE, getHeights()));
+        Mockito.doReturn(true).when(fragment).isVisible();
         activity.getSupportFragmentManager().beginTransaction().add(fragment, "Height-for-Age-Boys").commitNow();
         View view = fragment.getView();
         Assert.assertNotNull(view);
@@ -45,5 +61,17 @@ public class HeightMonitoringFragmentTest extends BaseUnitTest {
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.age), ((CustomFontTextView)constraintLayout.findViewById(R.id.column_one_metric)).getText());
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.height), ((CustomFontTextView)constraintLayout.findViewById(R.id.metric_label)).getText());
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.z_score), ((CustomFontTextView)constraintLayout.findViewById(R.id.column_three_metric)).getText());
+    }
+
+    private HeightZScore getHeightZScore(int age) {
+        HeightZScore heightZScore = new HeightZScore();
+        heightZScore.setGender(Gender.MALE);
+        heightZScore.setMonth(age);
+        return heightZScore;
+    }
+
+    @After
+    public void destroy(){
+        ReflectionHelpers.setField(GrowthMonitoringLibrary.getInstance(), "heightZScoreRepository", null);
     }
 }
--- opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/WeightMonitoringFragmentTest.java
+++ opensrp-growth-monitoring/src/test/java/org/smartregister/growthmonitoring/fragment/WeightMonitoringFragmentTest.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.view.View;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +13,16 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.opensrp.api.constants.Gender;
 import org.robolectric.Robolectric;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.growthmonitoring.BaseUnitTest;
+import org.smartregister.growthmonitoring.GrowthMonitoringLibrary;
 import org.smartregister.growthmonitoring.R;
+import org.smartregister.growthmonitoring.domain.WeightZScore;
+import org.smartregister.growthmonitoring.repository.WeightZScoreRepository;
 import org.smartregister.view.customcontrols.CustomFontTextView;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ndegwamartin on 2020-04-15.
@@ -35,17 +43,38 @@ public class WeightMonitoringFragmentTest extends BaseUnitTest {
 
     @Test
     public void testThatWeightForAgeViewIsCreated() throws InterruptedException {
+        ArrayList<WeightZScore> weightZScores = new ArrayList<>();
+        weightZScores.add(getWeightZScore(56));
+        weightZScores.add(getWeightZScore(44));
+
+        WeightZScoreRepository  weightZScoreRepository = Mockito.spy(GrowthMonitoringLibrary.getInstance().weightZScoreRepository());
+        Mockito.doReturn(weightZScores).when(weightZScoreRepository).findByGender(Mockito.any());
+        ReflectionHelpers.setField(GrowthMonitoringLibrary.getInstance(), "weightZScoreRepository", weightZScoreRepository);
+
         WeightMonitoringFragment fragment = Mockito.spy(WeightMonitoringFragment.createInstance("2018-09-12", Gender.MALE, getWeights()));
+        Mockito.doReturn(true).when(fragment).isVisible();
         activity.getSupportFragmentManager().beginTransaction().add(fragment, "Weight-for-Age-Boys").commitNow();
         View view = fragment.getView();
         Assert.assertNotNull(view);
         Assert.assertTrue(view instanceof ConstraintLayout);
         ConstraintLayout constraintLayout = (ConstraintLayout) view;
-        Thread.sleep(3000);
+        TimeUnit.SECONDS.toMillis(3);
         Assert.assertEquals(3, constraintLayout.getChildCount());
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.age), ((CustomFontTextView) constraintLayout.findViewById(R.id.column_one_metric)).getText());
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.weight), ((CustomFontTextView) constraintLayout.findViewById(R.id.metric_label)).getText());
         Assert.assertEquals(constraintLayout.getContext().getString(R.string.z_score), ((CustomFontTextView) constraintLayout.findViewById(R.id.column_three_metric)).getText());
+    }
+
+    private WeightZScore getWeightZScore(int age) {
+        WeightZScore weightZScore = new WeightZScore();
+        weightZScore.setGender(Gender.MALE);
+        weightZScore.setMonth(age);
+        return weightZScore;
+    }
+
+    @After
+    public void destroy(){
+        ReflectionHelpers.setField(GrowthMonitoringLibrary.getInstance(), "weightZScoreRepository", null);
     }
 
 }
